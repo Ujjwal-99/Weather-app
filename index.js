3ef1f24e49c642716bd19bbb188285d8f8f1566a
UJJWAL BHATT <ujjwalbhatt180180@gmail.com>
2023-04-29 18:12:55 +0530

    app created

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const container= document.querySelector('container');
+const container= document.querySelector('.container');
 const error= document.querySelector('.error');
-const weatherBox= document.querySelector('weather-box');
+const weatherBox= document.querySelector('.weather-box');
 const weatherDetail= document.querySelector('.weather-detail');
 const search= document.querySelector('.search-box');
 
 search.addEventListener('click',()=>{
     const key ='30836e0b5875d209156e233a3cfd5309';
-    const city= document.querySelector('.search-box input').Value;
+    const city= document.querySelector('.search-box input').value;
    
     if (city==='') return;
    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`).then((Response)=>Response.json()).then((json) => {
-        if(json.cod==='404'){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`).then((response)=>response.json()).then((json) => {
+        if(json.cod==='404')
+        {
         container.style.height='505px';
-        weatherBox,.style.display='none';
+        weatherBox.style.display='none';
         weatherDetail.style.display='none';
         error.style.display='block';
         return;
         }
+
     error.style.display='none';
    
     const img= document.querySelector('.weather-box img');
-    const temperature=document.querySelector('.weather-box.temp');
-    const description=document.querySelector('.weather-box.disc');
-    const humidity=document.querySelector('.weather-detail.humidity span');
-    const wind=document.querySelector('.weather-detail.speed span ');
+    const temp=document.querySelector('.weather-box .temp');
+    const description=document.querySelector('.weather-box .disc');
+    const humidity=document.querySelector('.weather-detail .humidity span');
+    const wind=document.querySelector('.weather-detail .speed span ');
    
    
     switch(json.weather[0].main){
@@ -49,11 +51,11 @@ search.addEventListener('click',()=>{
     }
    
     temp.innerHTML=`${json.main.temp}<span>&degC</span>`;
-    disc.innerHTML=`${json..weather[0].description}`;
-    humidity.innerHTML=`${json.maiin.humidity}%`;
-    speed.innerHTML=`${json.wind.speed}km/h`;
+    description.innerHTML=`${json.weather[0].description}`;
+    humidity.innerHTML=`${json.main.humidity}%`;
+    wind.innerHTML=`${json.wind.speed}km/h`;
    
-    container.style.height='505px';
+    container.style.height='605px';
     weatherBox.style.display='Block';
     weatherDetail.style.display='flex';
 })
